690ff5b4eb897b7694827d7f77502014f164e399
jsburke <jburke@draper.com>
2018-06-13 12:20:58 -0400

    prints, fn name

--- e_math/e_math.c
+++ e_math/e_math.c
@@ -1,11 +1,11 @@
-int fn(int a){
-  return (a < 2) ? 1 : a*fn(a-1);
+f(int a){
+  return (a < 2) ? 1 : a*f(a-1);
 }
 
 main(){
 float e = 0;
 
-for(int n = 0; n < 10; n++) e += 1.0 / fn(n);
+for(int n=0;n<10;n++)e+=1.0/f(n);
 
-printf("e: %f", e);
+printf("%f", e);
 }

3426e0da1b9df52622ff5d1211dbae79848a4c3a
jsburke <jburke@draper.com>
2018-06-13 12:09:02 -0400

    tighten recursive call, remove void from main

--- e_math/e_math.c
+++ e_math/e_math.c
@@ -1,9 +1,8 @@
 int fn(int a){
-  if (a < 2) return 1;
-  return a*fn(a-1);
+  return (a < 2) ? 1 : a*fn(a-1);
 }
 
-void main(){
+main(){
 float e = 0;
 
 for(int n = 0; n < 10; n++) e += 1.0 / fn(n);

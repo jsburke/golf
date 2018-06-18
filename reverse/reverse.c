b1c683c8545215802035a717adb72b04d17b6d08
jsburke <jburke@draper.com>
2018-06-18 11:55:24 -0400

    ungummed it

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -1,6 +1,18 @@
 #include"d.h"
-i,e,t,l;main(){
-while(a[e++]);
-while(i<e){t=a[i];a[i++]=a[e];a[e--]=t;}
-for(i=0;i<l;)printf("%c\n",a[i++]);
+i,e,t;
+
+void reverse(){
+  while(i<e){
+  printf("a[%d] == %c || a[%d] == %c\n", i, a[i], e, a[e]);
+  t=a[i];
+  a[i++]=a[e];
+  a[e--]=t;
+  }
+}
+
+main(){
+while(a[++e]);
+e--;
+reverse();
+printf("%s\n",a);
 }

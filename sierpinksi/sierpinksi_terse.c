0020dce3325b73c518af3cce013db1766ac262f1
jsburke <jburke@draper.com>
2019-04-25 12:55:35 -0400

    coallesce loops

--- sierpinksi/sierpinksi_terse.c
+++ sierpinksi/sierpinksi_terse.c
@@ -1,9 +1,6 @@
 x,y,i,s=1<<5;
 main()
 {
-	for (y = s - 1; y+1; y--, printf("\n")) {
-		for (x=i=0; i++ < y;) printf(" ");
-		for (; x + y < s;)
-			printf(x++ & y ? "  " : "* ");
-	}
+	for (y = s - 1; y+1; y--, printf("\n"))
+		for (x=i=0; i < y || x+y<s;) printf(i++<y?" ":x++&y?"  ":"* ");
 }

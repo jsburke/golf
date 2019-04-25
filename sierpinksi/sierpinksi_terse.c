f867117821420fece514cd1368e1116ab58535f2
jsburke <jburke@draper.com>
2019-04-25 12:12:58 -0400

    move inc to postfix in condition

--- sierpinksi/sierpinksi_terse.c
+++ sierpinksi/sierpinksi_terse.c
@@ -2,7 +2,7 @@ x,y,i,s=1<<5;
 main()
 {
 	for (y = s - 1; y >= 0; y--, printf("\n")) {
-		for (i = 0; i < y; i++) printf(" ");
+		for (i = 0; i++ < y;) printf(" ");
 		for (x = 0; x + y < s; x++)
 			printf((x & y) ? "  " : "* ");
 	}

3b291e5a70e8013a92dd502e898ac2128a08b4f8
jsburke <jburke@draper.com>
2019-04-25 13:16:47 -0400

    trim whitespace, prov final ~100 bytes

--- sierpinksi/sierpinksi_terse.c
+++ sierpinksi/sierpinksi_terse.c
@@ -1,6 +1,1 @@
-x,y,i,s=1<<5;
-main()
-{
-	for (y = s - 1; y+1; y--, puts(""))
-		for (x=i=0; i < y | x+y<s;) printf(i++<y?" ":x++&y?"  ":"* ");
-}
+x,y,i,s=1<<5;main(){for(y=s-1;y+1;y--,puts(""))for(x=i=0;i<y|x+y<s;)printf(i++<y?" ":x++&y?"  ":"* ");}

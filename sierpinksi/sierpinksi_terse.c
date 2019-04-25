c6e73793b32edec7b9c701ada95eb01bd7ed4e6e
jsburke <jburke@draper.com>
2019-04-25 12:33:47 -0400

    compress loop inits

--- sierpinksi/sierpinksi_terse.c
+++ sierpinksi/sierpinksi_terse.c
@@ -2,8 +2,8 @@ x,y,i,s=1<<5;
 main()
 {
 	for (y = s - 1; y+1; y--, printf("\n")) {
-		for (i = 0; i++ < y;) printf(" ");
-		for (x = 0; x + y < s;)
+		for (x=i=0; i++ < y;) printf(" ");
+		for (; x + y < s;)
 			printf((x++ & y) ? "  " : "* ");
 	}
 }

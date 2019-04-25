89525bafb7dbbe1738962d25715efad970df58fa
jsburke <jburke@draper.com>
2019-04-25 12:59:32 -0400

    logical and bitwise or plus precedence shaves a bit

--- sierpinksi/sierpinksi_terse.c
+++ sierpinksi/sierpinksi_terse.c
@@ -2,5 +2,5 @@ x,y,i,s=1<<5;
 main()
 {
 	for (y = s - 1; y+1; y--, printf("\n"))
-		for (x=i=0; i < y || x+y<s;) printf(i++<y?" ":x++&y?"  ":"* ");
+		for (x=i=0; i < y | x+y<s;) printf(i++<y?" ":x++&y?"  ":"* ");
 }

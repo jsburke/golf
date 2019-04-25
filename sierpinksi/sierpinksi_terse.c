ae31964ba946632b3f8c489475e5629534384d81
jsburke <jburke@draper.com>
2019-04-25 12:51:25 -0400

    delete parens

--- sierpinksi/sierpinksi_terse.c
+++ sierpinksi/sierpinksi_terse.c
@@ -4,6 +4,6 @@ main()
 	for (y = s - 1; y+1; y--, printf("\n")) {
 		for (x=i=0; i++ < y;) printf(" ");
 		for (; x + y < s;)
-			printf((x++ & y) ? "  " : "* ");
+			printf(x++ & y ? "  " : "* ");
 	}
 }

1f543ed9363bbc374205348717a543bb6c3fac96
jsburke <jburke@draper.com>
2019-04-25 12:17:22 -0400

    shave a byte since y>=0 == y+1 for logical cond

--- sierpinksi/sierpinksi_terse.c
+++ sierpinksi/sierpinksi_terse.c
@@ -1,7 +1,7 @@
 x,y,i,s=1<<5;
 main()
 {
-	for (y = s - 1; y >= 0; y--, printf("\n")) {
+	for (y = s - 1; y+1; y--, printf("\n")) {
 		for (i = 0; i++ < y;) printf(" ");
 		for (x = 0; x + y < s;)
 			printf((x++ & y) ? "  " : "* ");

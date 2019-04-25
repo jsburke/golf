b31878a0816feafa0a8b7079e1a9ba40d51d917a
jsburke <jburke@draper.com>
2019-04-25 12:13:49 -0400

    move other postfix inc

--- sierpinksi/sierpinksi_terse.c
+++ sierpinksi/sierpinksi_terse.c
@@ -3,7 +3,7 @@ main()
 {
 	for (y = s - 1; y >= 0; y--, printf("\n")) {
 		for (i = 0; i++ < y;) printf(" ");
-		for (x = 0; x + y < s; x++)
-			printf((x & y) ? "  " : "* ");
+		for (x = 0; x + y < s;)
+			printf((x++ & y) ? "  " : "* ");
 	}
 }

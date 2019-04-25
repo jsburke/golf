7cc6847fc1a48e6a640e5f0a8d5a2a08972da95e
jsburke <jburke@draper.com>
2019-04-25 12:08:58 -0400

    remove include, compress sizing into global

--- sierpinksi/sierpinksi_terse.c
+++ sierpinksi/sierpinksi_terse.c
@@ -1,12 +1,10 @@
-#include <stdio.h>
-#define N     5 
-#define SIZE (1 << N)
+s=1<<5;
 int main()
 {
 	int x, y, i;
-	for (y = SIZE - 1; y >= 0; y--, printf("\n")) {
+	for (y = s - 1; y >= 0; y--, printf("\n")) {
 		for (i = 0; i < y; i++) printf(" ");
-		for (x = 0; x + y < SIZE; x++)
+		for (x = 0; x + y < s; x++)
 			printf((x & y) ? "  " : "* ");
 	}
 	return 0;

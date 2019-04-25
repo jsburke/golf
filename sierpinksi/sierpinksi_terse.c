095ce2a90d2b6f6dc0ff960f33b28d53d2ac1a54
jsburke <jburke@draper.com>
2019-04-25 13:21:16 -0400

    reorder to put n for steps at front

--- sierpinksi/sierpinksi_terse.c
+++ sierpinksi/sierpinksi_terse.c
@@ -1,1 +1,1 @@
-x,y,i,s=1<<5;main(){for(y=s-1;y+1;y--,puts(""))for(x=i=0;i<y|x+y<s;)printf(i++<y?" ":x++&y?"  ":"* ");}
+s=1<<5,x,y,i;main(){for(y=s-1;y+1;y--,puts(""))for(x=i=0;i<y|x+y<s;)printf(i++<y?" ":x++&y?"  ":"* ");}

b06be2c34bc494ba7a14c06e027b1c6349039bdd
jsburke <jburke@draper.com>
2019-04-25 12:11:38 -0400

    vars on one line, remove int from main, remove return

--- sierpinksi/sierpinksi_terse.c
+++ sierpinksi/sierpinksi_terse.c
@@ -1,11 +1,9 @@
-s=1<<5;
-x,y,i;
-int main()
+x,y,i,s=1<<5;
+main()
 {
 	for (y = s - 1; y >= 0; y--, printf("\n")) {
 		for (i = 0; i < y; i++) printf(" ");
 		for (x = 0; x + y < s; x++)
 			printf((x & y) ? "  " : "* ");
 	}
-	return 0;
 }

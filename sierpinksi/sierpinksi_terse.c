c86dd5e6434d42a1e58252a8d7728bc77323c3db
jsburke <jburke@draper.com>
2019-04-25 21:34:45 -0400

    properly coallesce i and x

--- sierpinksi/sierpinksi_terse.c
+++ sierpinksi/sierpinksi_terse.c
@@ -1,1 +1,1 @@
-s=2<<5,x,i;main(y){for(y=s;y--;puts(""))for(x=i=0;x+y<s;)printf(i++<y?" ":x++&y?"  ":"* ");}
+s=2<<5,x;main(y){for(y=s;y--;puts(""))for(x=0;x<s;)printf(x++<y?" ":x-y-1&y?"  ":"* ");}

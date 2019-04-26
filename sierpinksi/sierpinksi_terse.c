33d9537e10faa8c6ebde71b4858c585fd52c1d62
jsburke <jburke@draper.com>
2019-04-25 20:35:03 -0400

    tricks on y from reddit codegolf

--- sierpinksi/sierpinksi_terse.c
+++ sierpinksi/sierpinksi_terse.c
@@ -1,1 +1,1 @@
-s=2<<5,x,y,i;main(){for(y=s-1;y+1;y--,puts(""))for(x=i=0;i<y|x+y<s;)printf(i++<y?" ":x++&y?"  ":"* ");}
+s=2<<5,x,i;main(y){for(y=s;y--;puts(""))for(x=i=0;i<y|x+y<s;)printf(i++<y?" ":x++&y?"  ":"* ");}

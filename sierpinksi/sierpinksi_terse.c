cf4189f69d5e7cd396a7a61c85deae097d506194
jsburke <jburke@draper.com>
2019-04-25 21:28:55 -0400

    prev was in error, this fixes and cuts

--- sierpinksi/sierpinksi_terse.c
+++ sierpinksi/sierpinksi_terse.c
@@ -1,1 +1,1 @@
-s=2<<5,x;main(y){for(y=s;y--;puts(""))for(x=0;x<y|x++<s;)printf(x<y?" ":x-y&y?"  ":"* ");}
+s=2<<5,x,i;main(y){for(y=s;y--;puts(""))for(x=i=0;x+y<s;)printf(i++<y?" ":x++&y?"  ":"* ");}
